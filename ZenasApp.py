08ecc4a13ae78eb284a0f58602461e0606a53686
jivanimehul <jivani28@gmail.com>
2024-09-21 19:38:38 +0530

    Update ZenasApp.py

--- ZenasApp.py
+++ ZenasApp.py
@@ -13,28 +13,8 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 #get a list of colors for a drop list selection
-#table_colors = session.sql("select color_or_style from catalog_for_website")
-#pd_colors = table_colors.to_pandas();
-
-
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-#st.dataframe(data=my_datafrmae, use_container_width=True)
-#st.stop()
-
-#convert the snowpark Dataframe to a pandas Dataframe so we can use the LOC function
-pd_df = my_dataframe.to_pandas()
-#st.dataframe(pd_df)
-#st.stop()
-
-ingredients_list = st.multiselect('Choose up to 5 ingredients:'
-                                  , my_dataframe
-                                  , max_selections=5)
-
-
-
-
-
-
+table_colors = session.sql("select color_or_style from catalog_for_website")
+pd_colors = table_colors.to_pandas();
 
 
 
