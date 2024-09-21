af4445a6b8e03c894da842a4f38fdb5b3ca4b3c1
jivanimehul <jivani28@gmail.com>
2024-09-21 19:29:41 +0530

    Update ZenasApp.py

--- ZenasApp.py
+++ ZenasApp.py
@@ -17,6 +17,31 @@ session = cnx.session()
 #pd_colors = table_colors.to_pandas();
 
 
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+#st.dataframe(data=my_datafrmae, use_container_width=True)
+#st.stop()
+
+#convert the snowpark Dataframe to a pandas Dataframe so we can use the LOC function
+pd_df = my_dataframe.to_pandas()
+#st.dataframe(pd_df)
+#st.stop()
+
+ingredients_list = st.multiselect('Choose up to 5 ingredients:'
+                                  , my_dataframe
+                                  , max_selections=5)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ##Oyt the list of colors into a drop list selector
 #option = st.selectbox('Pick a sweatsuit color or style:',pd_colors)
